8bf1de620b7c9a6134275b47304fbd8a9e787ef1
AvenaQuaker <jhonesbortoni@gmail.com>
2024-04-25 01:05:49 -0500

    TerminacionGraficas

--- Public/Dashboard.js
+++ Public/Dashboard.js
@@ -287,36 +287,6 @@ let array2 = [
         }
     ];
 
-var data1 = [
-    { y: 'Enero', a: 10},
-    { y: 'Febrero', a: 10},
-    { y: 'Marzo', a: 10},
-    { y: 'Abril', a: 10},
-    { y: 'Mayo', a: 10},
-    { y: 'Junio', a: 10},
-    { y: 'Julio', a: 10},
-    { y: 'Agosto', a: 19,},
-    { y: 'Septiembre', a: 10},
-    { y: 'Octubre', a: 10,},
-    { y: 'Noviembre', a: 10},
-    { y: 'Diciembre', a: 10}
-]
-
-var data2 = [
-    { y: 'Enero', a: 10000},
-    { y: 'Febrero', a: 20000},
-    { y: 'Marzo', a: 50000},
-    { y: 'Abril', a: 100000},
-    { y: 'Mayo', a: 150000},
-    { y: 'Junio', a: 300000},
-    { y: 'Julio', a: 200000},
-    { y: 'Agosto', a: 180000,},
-    { y: 'Septiembre', a: 100000},
-    { y: 'Octubre', a: 50000,},
-    { y: 'Noviembre', a: 30000},
-    { y: 'Diciembre', a: 20000}
-]
-
 //Obtencion de Contenedores
 let page1 = document.getElementById('page1')
 let page2 = document.getElementById('page2')
@@ -736,67 +706,90 @@ let table2 = new DataTable('#tabla2', {
 
 //Generacion de las Graficas mediante los datos (data)
 config1 = {
-    element: 'bar-chart1',
-    data: data1,
-    xkey: 'y',
-    ykeys: ['a'],
-    labels: ['Dias Rentados'],
-    fillOpacity: 0.6,
-    hideHover: 'auto',
-    behaveLikeLine: true,
-    resize: true,
-    pointFillColors:['#ffffff'],
-    pointStrokeColors: ['black'],
-    barColors:['#008cff','black']
-}; Morris.Bar(config1);
-
-
-config2 = {
-    element:'bar-chart2',
-    data: data2,
-    xkey: 'y',
-    ykeys: ['a'],
-    labels: ['Dias Rentados'],
-    fillOpacity: 0.6,
-    hideHover: 'auto',
-    behaveLikeLine: true,
-    resize: true,
-    pointFillColors:['#ffffff'],
-    pointStrokeColors: ['black'],
-    barColors:['#002aff','black']
-}; Morris.Bar(config2);
-
-config3 = {
+    resize:true,
     element: 'line-chart',
     data: [
-        { month: '2012-01', value: 20 },
-        { month: '2012-02', value: 10 },
-        { month: '2012-03', value: 5 },
-        { month: '2012-04', value: 5 },
-        { month: '2012-05', value: 20 },
-        { month: '2012-06', value: 20 },
-        { month: '2012-07', value: 10 },
-        { month: '2012-08', value: 5 },
-        { month: '2012-09', value: 5 },
-        { month: '2012-10', value: 20 },
-        { month: '2012-11', value: 20 },
-        { month: '2012-12', value: 20 }
+        { month: '2024-01', value: 20 },
+        { month: '2024-02', value: 10 },
+        { month: '2024-03', value: 5 },
+        { month: '2024-04', value: 5 },
+        { month: '2024-05', value: 20 },
+        { month: '2024-06', value: 20 },
+        { month: '2024-07', value: 10 },
+        { month: '2024-08', value: 5 },
+        { month: '2024-09', value: 5 },
+        { month: '2024-10', value: 20 },
+        { month: '2024-11', value: 20 },
+        { month: '2024-12', value: 20 }
     ],
     xkey: 'month',
     ykeys: ['value'],
-    labels: ['Value']
-}; Morris.Line(config3);
+    labels: ['Ganancias Totales'],
+    lineColors: ['darkGreen'],
+    pointFillColors: ['lightGreen']
+}; Morris.Line(config1);
+
+config2 = {
+    resize:true,
+    element: 'area-chart',
+    data: [
+        { month: '2024-01', a: 5, b:7, c:12 },
+        { month: '2024-02', a: 10, b:9,c:11},
+        { month: '2024-03', a: 5 ,b:8,c:6 },
+        { month: '2024-04', a: 11 ,b:9,c:7 },
+        { month: '2024-05', a: 9,b:10 ,c:11 },
+        { month: '2024-06', a: 7,b:6 ,c:12 },
+        { month: '2024-07', a: 13,b:7 ,c:5 },
+        { month: '2024-08', a: 6, b:8 ,c:16},
+        { month: '2024-09', a: 6 ,b:9,c:7 },
+        { month: '2024-10', a: 11, b:10 ,c:9},
+        { month: '2024-11', a: 10,b:7,c:11  },
+        { month: '2024-12', a: 7 ,b:15,c:6 }
+    ],
+    xkey: 'month',
+    ykeys: ['a','b','c'],
+    labels: ['Lunes-Jueves','Viernes','Fin de Semana'],
+    lineColors: ['#035a7c','#002488','#3b0088'],
+    ymax: 40,
+}; Morris.Area(config2);
+
+
+config3 = {
+    resize:true,
+    element: 'bar-chart',
+    data: [
+        { y: 'Enero', a: 15000, b: 20000, c: 10000},
+        { y: 'Febrero', a: 22000, b: 18000, c: 25000},
+        { y: 'Marzo', a: 28000, b: 16000, c: 21000},
+        { y: 'Abril', a: 12000, b: 27000, c: 19000},
+        { y: 'Mayo', a: 25000, b: 14000, c: 23000},
+        { y: 'Junio', a: 17000, b: 24000, c: 20000},
+        { y: 'Julio', a: 19000, b: 22000, c: 27000},
+        { y: 'Agosto', a: 23000, b: 19000, c: 18000},
+        { y: 'Septiembre', a: 20000, b: 25000, c: 17000},
+        { y: 'Octubre', a: 16000, b: 21000, c: 24000},
+        { y: 'Noviembre', a: 21000, b: 17000, c: 22000},
+        { y: 'Diciembre', a: 18000, b: 23000, c: 16000}
+    ],
+    xkey: 'y',
+    ykeys: ['a','b','c'],
+    labels: ['Servicios','Mantenimiento','Productos'],
+    pointFillColors:['#ffffff'],
+    pointStrokeColors: ['black'],
+    barColors:['darkred','#b90000','#ff3700'],
+    stacked: true,
+}; Morris.Bar(config3);
+
 
 config4 = {
-    resize:false,
+    resize:true,
     element: 'donut-chart',
     data: [
-    {label: "Lunes", value: 5},
-    {label: "Martes", value: 5},
-    {label: "Miercoles", value: 5},
-    {label: "Jueves", value: 10},
-    {label: "Viernes", value: 25},
-    {label: "Sabado", value: 20},
-    {label: "Domingo", value: 30}
-    ]
+    {label: "PayPal", value: 15},
+    {label: "Mastercard", value: 45},
+    {label: "Visa", value: 25},
+    {label: "Oxxo", value: 10},
+    {label: "GooglePlay", value: 5},
+    ],
+    formatter: function (y, data) { return y + '%' },
 }; Morris.Donut(config4);
